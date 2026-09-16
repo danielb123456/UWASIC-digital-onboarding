6755aaa12aae5fed0f9a705516afaa3400252e2a
danielb123456 <daniel.bondar405@gmail.com>
2026-09-15 22:17:25 -0400

    Fixed project structure

---
+++ src/project.v
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Daniel Bondar
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_uwasic_onboarding_danielb123456 (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  assign uio_oe = 8'hFF; // Set all IOs to output
+
+  // TODO: Connect these register values to the SPI peripheral when it is added.
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
+
+  pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
+
+endmodule
