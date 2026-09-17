51e1cb1d93e3657b3429774d1651cf1c66d3dd97
danielb123456 <daniel.bondar405@gmail.com>
2026-09-17 00:53:34 -0400

    also changed project.v

--- src/project.v
+++ src/project.v
@@ -17,13 +17,22 @@ module tt_um_uwasic_onboarding_danielb123456 (
 );
 
   assign uio_oe = 8'hFF; // Set all IOs to output
-
-  // TODO: Connect these register values to the SPI peripheral when it is added.
   wire [7:0] en_reg_out_7_0;
   wire [7:0] en_reg_out_15_8;
   wire [7:0] en_reg_pwm_7_0;
   wire [7:0] en_reg_pwm_15_8;
   wire [7:0] pwm_duty_cycle;
+
+  spi_peripheral spi_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .ui_in(ui_in[2:0]),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle)
+  );
 
   pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
